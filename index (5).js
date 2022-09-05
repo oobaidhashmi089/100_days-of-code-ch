a267bbd9d0e08ed5fe56b4181928a7f032e483dd
Syed Obaid Ullah Hashmi <79754399+oobaidhashmi089@users.noreply.github.com>
2022-09-05 22:40:48 +0500

    day8 Kadane's Algorithm & The Maximum Subarray
    day8 Kadane's Algorithm & The Maximum Subarray Problem

---
+++ index (5).js
@@ -0,0 +1,18 @@
+
+////Kadane's Algorithm & The Maximum Subarray Problem
+
+var maxSubArray = function(nums) {
+  if (nums.length == 0)
+    return 0;
+  let max = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] > 0) {
+      nums[i] += nums[i - 1];
+    }
+    max = Math.max(nums[i], max);
+  }
+  return max;
+};
+
+
+
